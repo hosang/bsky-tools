7cd3ec247fec2248ada0f3e049b71df297741c81
Jan <jan.hosang@gmail.com>
2023-11-26 16:42:21 +0000

    Feed publishing tool.

---
+++ config.example.py
@@ -0,0 +1,21 @@
+import dataclasses
+
+@dataclasses.dataclass
+class Feed:
+    record_name: str
+    display_name: str
+    description: str
+    emoji: list[str]
+    hashtags: list[str]
+
+# Service
+HOSTNAME = 'bsky.komagomama.com'
+SERVICE_DID = f'did:web:{HOSTNAME}'
+
+# Account
+HANDLE = 'user.bsky.social'
+PASSWORD = 'xxxx-xxxx-xxxx-xxxx'
+
+# Feeds
+FEED_URI = 'at://did:plc:..../app.bsky.feed.generator/'
+FEEDS = []
